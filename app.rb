14e601fd86d7dfaedeb09c9ae91d9a01a0fecf7f
jeremy & peter <student@epicodus.com>
2015-04-28 10:40:50 -0700

    It will run in sinatra now

---
+++ app.rb
@@ -0,0 +1,16 @@
+require('sinatra')
+require('sinatra/reloader')
+also_reload('lib/**/*.rb')
+require('./lib/task')
+
+get('/') do
+  @list = Task.all
+  erb(:index)
+end
+
+post('/tasks') do
+  item = params.fetch('list')
+  add_task = Task.new(item)
+  add_task.store()
+erb(:success)
+end
